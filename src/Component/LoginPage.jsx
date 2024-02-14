0a81a210fcd5dffeaa4fa263c402f409495b0a7c
Shubham Gupta <guptashubham5991@gmail.com>
2024-02-14 12:33:05 +0530

    project done

--- src/Component/LoginPage.jsx
+++ src/Component/LoginPage.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+    const [userName,setUserName] = useState('');
+    const [password,setPassword] = useState('');
+    const [error,setError]   = useState('')
+    const [visible,setVisible] = useState(false)
+
+    function handleSubmit(e){
+        e.preventDefault();
+       if (userName==='user' && password==='password'){
+        setError('');
+        setVisible(true)
+       }else{
+        setVisible(false);
+        setError('Invalid username or password')
+       }
+
+    }
+
   return (
     <div>
         <h1>Login Page</h1>
-        <form action="">
+        {
+            visible?(<p>Welcome,{userName}!</p>):(
+                
+                <form onSubmit={handleSubmit}>
+                    <p>{error}</p>
             <div>
             <label htmlFor="name">Username:</label>
-            <input type="text"  name='name' required/>
+            <input type="text"  id='name' required value={userName} onChange={(e)=>setUserName(e.target.value)}/>
             </div>
             <div>
             <label htmlFor="password">Password:</label>
-            <input type="password"  name='password' required/>
+            <input type="password"  id='password' required value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
             <button type='submit'>Submit</button>
            
         </form>
+            )
+        }
+        
     </div>
   )
 }
